ed891a709a81f595c15eb3175a5fbcc4d3f1b0d5
Aleksas Kaminskas <aleksiuxx@gmail.com>
2023-01-10 22:16:02 +0200

    Changing tank function check_enemy

--- Level2/tank.py
+++ Level2/tank.py
@@ -6,6 +6,7 @@ class Tank:
     def __init__(self):
         self.x = 0
         self.y = 0
+        self.score = 300
         self.shots = 0
         self.position = 'n'
         self.positions = {'n': 'North', 'e': 'East', 's': 'South', 'w': 'West'}
@@ -42,6 +43,41 @@ class Tank:
         print(f"Tank has made:{self.shots} shot's")
         time.sleep(2)
 
+    # def check_enemy(self):
+    #     print(f"Target - X:{self.x}, Y:{self.y}")
+    #     if self.x == (enemy.x - 1) and self.y == self.y and tank.position == 'e':
+    #         print('Nice job you killed target.')
+    #         return True
+    #     elif tank.x == self.x and tank.y == (self.y - 1) and tank.position == 'n':
+    #         print('Nice job you killed target.')
+    #         return True
+    #     elif tank.x == (self.x + 1) and tank.y == self.y and tank.position == 'w':
+    #         print('Nice job you killed target.')
+    #         return True
+    #     elif tank.x == self.x and tank.y == (self.y + 1) and tank.position == 's':
+    #         print('Nice job you killed target.')
+    #         return True
+    #     else:
+    #         print('You missed.')
+    #         return False
+
+    def check_enemy(self):
+        print(f"Target - X:{self.x}, Y:{self.y}")
+        if self.x == enemy.x:
+            if self.y < enemy.y and self.position == 'n':
+                print('Nice job you killed target.')
+                return True
+            if self.y > enemy.y and self.position == 's':
+                print('Nice job you killed target.')
+                return True
+            return False
+        if self.y == enemy.y:
+            if self.x > enemy.x and self.position == 'e':
+                return True
+            if self.x < enemy.x and self.position == 'w':
+                return True
+            return False
+
 
 class Enemy(Tank):
     def __init__(self):
@@ -49,66 +85,40 @@ class Enemy(Tank):
         self.x = random.randint(-5, 6)
         self.y = random.randint(-5, 6)
 
-    def check_enemy(self):
-        print(f"Target - X:{self.x}, Y:{self.y}")
-        if tank.x == (self.x - 1) and tank.y == self.y and tank.position == 'e':
-            print('Nice job you killed target.')
-            self.x = random.randint(-5, 6)
-            self.y = random.randint(-5, 6)
-            return True
-        elif tank.x == self.x and tank.y == (self.y - 1) and tank.position == 'n':
-            print('Nice job you killed target.')
-            self.x = random.randint(-5, 6)
-            self.y = random.randint(-5, 6)
-            return True
-        elif tank.x == (self.x + 1) and tank.y == self.y and tank.position == 'w':
-            print('Nice job you killed target.')
-            self.x = random.randint(-5, 6)
-            self.y = random.randint(-5, 6)
-            return True
-        elif tank.x == self.x and tank.y == (self.y + 1) and tank.position == 's':
-            print('Nice job you killed target.')
-            self.x = random.randint(-5, 6)
-            self.y = random.randint(-5, 6)
-            return True
-        else:
-            print('You missed.')
-            return False
-
 
 side = []
 tank = Tank()
 enemy = Enemy()
-score = 300
+
 counter = 0
-while True:
-    print(score)
-    if score <= 0:
-        print("Game ended.")
-        break
+while tank.score > 0:
+    print(tank.score)
     player = input("f,b,l,r. 5-shoot, 1.-info\n-")
     # player = input("Choose what do want to do with tank:\nGo forward-f\nGo backwards-b\nGo left-l\nGo "
     #                "right-r\nIf you want to shoot to your direction press 5.\n1.-Check info.\n0.Quit game\n-")
     if player == 'f':
         tank.forward()
-        score -= 10
+        tank.score -= 10
     elif player == 'b':
         tank.backwards()
-        score -= 10
+        tank.score -= 10
     elif player == 'l':
         tank.left()
-        score -= 10
+        tank.score -= 10
     elif player == 'r':
         tank.right()
-        score -= 10
+        tank.score -= 10
     elif player == '1':
         tank.info()
     elif player == '5':
         tank.shooting()
         if enemy.check_enemy():
-            score += 70
+            tank.score += 70
             counter += 1
-            print(f"You killed one and now your score is - {score} and you killed {counter} targets.")
+            enemy = Enemy()
+            print(f"You killed one and now your score is - {tank.score} and you killed {counter} targets.")
     elif player == '0':
-        print(f"You ended game with:{score} and {counter} kills")
+        print(f"You ended game with:{tank.score} and {counter} kills")
         break
+
+print(f"You ended game with:{tank.score} and {counter} kills")
